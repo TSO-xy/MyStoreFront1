86c516e42e295e853d8fd6effce96becbfc62d66
JR Lim <josh.r.lim@gmail.com>
2018-02-26 14:24:09 -0600

    Added some default DbInitializer products

--- MyStoreFront1/DbInitializer.cs
+++ MyStoreFront1/DbInitializer.cs
@@ -37,34 +37,58 @@ namespace MyStoreFront1
                     DateCreated = DateTime.Now,
                     DateLastModified = DateTime.Now
                 });
+                context.Genres.Add(new Genres
+                {
+                    Name = "FX",
+                    DateCreated = DateTime.Now,
+                    DateLastModified = DateTime.Now
+                });
             }
 
             if (!context.Products.Any())
             {
                 context.Products.Add(new Products
                 {
-                    Name = "Jazz Pack",
-                    Price = 39.99m,
-                    Description = "Recreate the sharp, tangy sounds of Jazz music.",
+                    Name = "Jazz Strings",
+                    Price = 49.99m,
+                    Description = "A collection of silky smooth Jazz samples.",
                     ImageUrl = "/images/jazz.jpg",
                     DateCreated = DateTime.Now,
                     DateLastModified = DateTime.Now,
                 });
                 context.Products.Add(new Products
                 {
-                    Name = "Rock Pack",
-                    Price = 39.99m,
-                    Description = "Recreate the rugged sounds of Rock music.",
+                    Name = "Jazz Drums",
+                    Price = 29.99m,
+                    Description = "Recreate the sharp, tangy sounds of classic Jazz drumkits.",
+                    ImageUrl = "/images/jazzdrums.jpg",
+                    DateCreated = DateTime.Now,
+                    DateLastModified = DateTime.Now,
+                });
+                context.Products.Add(new Products
+                {
+                    Name = "Rock Drums",
+                    Price = 19.99m,
+                    Description = "Recreate the rugged drumkits of Rock music.",
                     ImageUrl = "/images/rock.jpg",
                     DateCreated = DateTime.Now,
                     DateLastModified = DateTime.Now,
                 });
                 context.Products.Add(new Products
                 {
-                    Name = "Hip Hop Pack",
-                    Price = 39.99m,
-                    Description = "Oldschool boom bap and new age 808s.",
+                    Name = "Boom Bap Pack",
+                    Price = 14.99m,
+                    Description = "Bring back the sounds of the Golden Age of Hip Hop.",
                     ImageUrl = "/images/hiphop.jpg",
+                    DateCreated = DateTime.Now,
+                    DateLastModified = DateTime.Now,
+                });
+                context.Products.Add(new Products
+                {
+                    Name = "808 Drums",
+                    Price = 14.99m,
+                    Description = "Full array of professional drum sounds for New Age Hip Hop",
+                    ImageUrl = "/images/808.png",
                     DateCreated = DateTime.Now,
                     DateLastModified = DateTime.Now,
                 });
